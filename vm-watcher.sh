c2bd0353f47fe32097c893cc3c12cef4f0d6291f
Tyler J Wilkinson <tjw218@psu.edu>
2016-03-14 15:12:01 -0400

    include shadow code for detecting Linux

--- vm-watcher.sh
+++ vm-watcher.sh
@@ -8,6 +8,10 @@ MAX=1
 # get the count of running virtual machines
 COUNT=`vagrant global-status | grep -c "running"`
 
+# TODO 
+# detect Linux
+# LINUX=`uname -a | grep -c Linux` 
+
 # show the user a warning if there is more than 1
 if [ "$COUNT" -gt "$MAX" ]; then
     osascript -e 'display notification "You are running multiple Virtual Machines. This may cause problems with VM accessibility." with title "Warning: Multiple VMs are Running!"'    
